7233d0e0d52b29180288d177927d50ed1cdfe93d
Tomasz Cudok <tomasz.cudok@justgiving.com>
2019-06-18 10:52:01 +0100

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  mode: 'development',
+  entry: {
+    client: ['./src/client'],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    port: 3000,
+    stats: 'errors-only',
+  },
+};
